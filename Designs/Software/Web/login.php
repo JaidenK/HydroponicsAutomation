4db9cac53d16c8bc8827179f6960a8dbe121082f
Jaiden King <jaaking@yahoo.com>
2018-11-26 17:23:24 -0800

    Start login page

---
+++ Designs/Software/Web/login.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Hydroponic Automation | Login</title>
+  <meta charset="utf-8">
+
+  <!-- Bootstrap 4 -->
+  <!-- Responsive Layout -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+
+  <!-- jQuery -->
+  <!-- Powerful JavaScript library -->
+  <script src="jquery-3.3.1.min.js"></script>
+
+</head>
+<body>
+
+<!-- Nav bar to replaced by jQuery and nav.html -->
+<div id="nav-placeholder"></div>
+<script>$(function(){$("#nav-placeholder").load("nav.html");});</script>
+
+<?php
+echo "My first PHP script!";
+?>
+
+<!-- Footer to replaced by jQuery and footer.html -->
+<div id="footer-placeholder"></div>
+<script>$(function(){$("#footer-placeholder").load("footer.html");});</script>
+
+
+</body>
+</html>
