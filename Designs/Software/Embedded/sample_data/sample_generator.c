c3f40d4c45c65516d34400846818565307cc8bbb
Jaiden King <jaaking@yahoo.com>
2019-01-22 17:02:09 -0800

    Sample data generation in C

--- Designs/Software/Embedded/sample_data/sample_generator.c
+++ Designs/Software/Embedded/sample_data/sample_generator.c
@@ -8,75 +8,142 @@
 
 void error(const char *msg) { perror(msg); exit(0); }
 
+struct SensorData {
+   double h2o_level;
+   double h2o_stored;
+   double ph_level;
+   double ph_up_stored;
+   double ph_down_stored;
+   double ec_level;
+   double ec_stored;
+   double temp_measured;
+   double flow_measured;
+   double flow_target;
+   double ph_target;
+   double ec_target;
+};
+
+void setRandomData(struct SensorData *sd) {
+   sd->h2o_level =       rand() % 256;
+   sd->h2o_stored =      rand() % 256;
+   sd->ph_level =        rand() % 256;
+   sd->ph_up_stored =    rand() % 256;
+   sd->ph_down_stored =  rand() % 256;
+   sd->ec_level =        rand() % 256;
+   sd->ec_stored =       rand() % 256;
+   sd->temp_measured =   rand() % 256;
+   sd->flow_measured =   rand() % 256;
+   sd->flow_target =     rand() % 256;
+   sd->ph_target =       rand() % 256;
+   sd->ec_target =       rand() % 256;
+}
+
+void getGETstr(char *buf, struct SensorData *sd) {
+   sprintf(
+      buf,
+      "ProductID=password&"
+      "h2o_level=%f&"
+      "h2o_stored=%f&"
+      "ph_level=%f&"
+      "ph_up_stored=%f&"
+      "ph_down_stored=%f&"
+      "ec_level=%f&"
+      "ec_stored=%f&"
+      "temp_measured=%f&"
+      "flow_measured=%f&"
+      "flow_target=%f&"
+      "ph_target=%f&"
+      "ec_target=%f",
+      sd->h2o_level,
+      sd->h2o_stored,
+      sd->ph_level,
+      sd->ph_up_stored,
+      sd->ph_down_stored,
+      sd->ec_level,
+      sd->ec_stored,
+      sd->temp_measured,
+      sd->flow_measured,
+      sd->flow_target,
+      sd->ph_target,
+      sd->ec_target
+   );
+}
+
 int main(int argc,char *argv[])
 {
-    /* first what are we going to send and where are we going to send it? */
-    int portno =        80;
-    char *host =        "api.somesite.com";
-    char *message_fmt = "POST /apikey=%s&command=%s HTTP/1.0\r\n\r\n";
+   struct SensorData *sd = malloc(sizeof(*sd));
+   setRandomData(sd);
+   char httpGETdata[2048];
+   getGETstr(httpGETdata,sd);
+   printf("%s\n",httpGETdata);
 
-    struct hostent *server;
-    struct sockaddr_in serv_addr;
-    int sockfd, bytes, sent, received, total;
-    char message[1024],response[4096];
+   /* first what are we going to send and where are we going to send it? */
+   int portno =        80;
+   char *host =        "sdp.ballistaline.com";
+   char *message_fmt = "GET http://sdp.ballistaline.com/dataReceiver.php?%s HTTP/1.0\r\n\r\n";
 
-    if (argc < 3) { puts("Parameters: <apikey> <command>"); exit(0); }
+   struct hostent *server;
+   struct sockaddr_in serv_addr;
+   int sockfd, bytes, sent, received, total;
+   char message[1024],response[4096];
 
-    /* fill in the parameters */
-    sprintf(message,message_fmt,argv[1],argv[2]);
-    printf("Request:\n%s\n",message);
+   if (argc < 2) { puts("Parameters: <GET>"); exit(0); }
 
-    /* create the socket */
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) error("ERROR opening socket");
+   /* fill in the parameters */
+   sprintf(message,message_fmt,httpGETdata);
+   printf("Request:\n%s\n",message);
 
-    /* lookup the ip address */
-    server = gethostbyname(host);
-    if (server == NULL) error("ERROR, no such host");
+   /* create the socket */
+   sockfd = socket(AF_INET, SOCK_STREAM, 0);
+   if (sockfd < 0) error("ERROR opening socket");
 
-    /* fill in the structure */
-    memset(&serv_addr,0,sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(portno);
-    memcpy(&serv_addr.sin_addr.s_addr,server->h_addr,server->h_length);
+   /* lookup the ip address */
+   server = gethostbyname(host);
+   if (server == NULL) error("ERROR, no such host");
 
-    /* connect the socket */
-    if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0)
-        error("ERROR connecting");
+   /* fill in the structure */
+   memset(&serv_addr,0,sizeof(serv_addr));
+   serv_addr.sin_family = AF_INET;
+   serv_addr.sin_port = htons(portno);
+   memcpy(&serv_addr.sin_addr.s_addr,server->h_addr,server->h_length);
 
-    /* send the request */
-    total = strlen(message);
-    sent = 0;
-    do {
-        bytes = write(sockfd,message+sent,total-sent);
-        if (bytes < 0)
-            error("ERROR writing message to socket");
-        if (bytes == 0)
-            break;
-        sent+=bytes;
-    } while (sent < total);
+   /* connect the socket */
+   if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0)
+     error("ERROR connecting");
 
-    /* receive the response */
-    memset(response,0,sizeof(response));
-    total = sizeof(response)-1;
-    received = 0;
-    do {
-        bytes = read(sockfd,response+received,total-received);
-        if (bytes < 0)
-            error("ERROR reading response from socket");
-        if (bytes == 0)
-            break;
-        received+=bytes;
-    } while (received < total);
+   /* send the request */
+   total = strlen(message);
+   sent = 0;
+   do {
+     bytes = write(sockfd,message+sent,total-sent);
+     if (bytes < 0)
+         error("ERROR writing message to socket");
+     if (bytes == 0)
+         break;
+     sent+=bytes;
+   } while (sent < total);
 
-    if (received == total)
-        error("ERROR storing complete response from socket");
+   /* receive the response */
+   memset(response,0,sizeof(response));
+   total = sizeof(response)-1;
+   received = 0;
+   do {
+     bytes = read(sockfd,response+received,total-received);
+     if (bytes < 0)
+         error("ERROR reading response from socket");
+     if (bytes == 0)
+         break;
+     received+=bytes;
+   } while (received < total);
 
-    /* close the socket */
-    close(sockfd);
+   if (received == total)
+     error("ERROR storing complete response from socket");
 
-    /* process response */
-    printf("Response:\n%s\n",response);
+   /* close the socket */
+   close(sockfd);
 
-    return 0;
+   /* process response */
+   printf("Response:\n%s\n",response);
+
+   return 0;
 }
---
+++ Designs/Software/Web/dataReceiver.php
@@ -0,0 +1,64 @@
+<?php
+
+include "dbinfo.php";
+
+// This all should be changed to a $_POST to increase security, reliability, and allow longer string.
+if($_GET['ProductID'] != "password") {
+   die("Invalid product id.");
+}
+
+
+$keys = array(
+   "h2o_level",
+   "h2o_stored",
+   "ph_level",
+   "ph_up_stored",
+   "ph_down_stored",
+   "ec_level",
+   "ec_stored",
+   "temp_measured",
+   "flow_measured",
+   "flow_target",
+   "ph_target",
+   "ec_target"
+);
+
+$isBadRequest = false; 
+
+$valuesString = "";
+foreach($keys as $key) {
+   if(!array_key_exists($key, $_GET)) {
+      echo $key." missing.\n";
+      $isBadRequest = true;
+   }
+}
+
+if($isBadRequest) {
+   die("Bad request.");
+}
+
+// TODO this needs to be cleaned up and refactored. Too much reppetitiotns 
+$h2o_level = (double)$_GET["h2o_level"];
+$h2o_stored = (double) $_GET["h2o_stored"];
+$ph_level = (double) $_GET["ph_level"];
+$ph_up_stored = (double) $_GET["ph_up_stored"];
+$ph_down_stored = (double) $_GET["ph_down_stored"];
+$ec_level = (double) $_GET["ec_level"];
+$ec_stored = (double) $_GET["ec_stored"];
+$temp_measured = (double) $_GET["temp_measured"];
+$flow_measured = (double) $_GET["flow_measured"];
+$flow_target = (double) $_GET["flow_target"];
+$ph_target = (double) $_GET["ph_target"];
+$ec_target = (double) $_GET["ec_target"];
+
+
+
+$sql = "INSERT INTO `TestTable` (`h2o_level`, `h2o_stored`, `ph_level`, `ph_up_stored`, `ph_down_stored`, `ec_level`, `ec_stored`, `temp_measured`, `flow_measured`, `flow_target`, `ph_target`, `ec_target`, `time`, `table_key`) VALUES ('$h2o_level', '$h2o_stored', '$ph_level', '$ph_up_stored', '$ph_down_stored', '$ec_level', '$ec_stored', '$temp_measured', '$flow_measured', '$flow_target', '$ph_target', '$ec_target', CURRENT_TIMESTAMP, NULL);";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+?> 
