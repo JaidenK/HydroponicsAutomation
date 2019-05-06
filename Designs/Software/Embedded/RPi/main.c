d1591c9a4fc30419c0177ff788eaf2880fdf6e48
bwong20 <bwong20@ucsc.edu>
2019-05-06 11:55:26 -0700

    Commiting changes before ui integration

--- Designs/Software/Embedded/RPi/main.c
+++ Designs/Software/Embedded/RPi/main.c
@@ -12,8 +12,30 @@
 #include "Protocol.h"
 #include "http.h"
 #include <time.h>
+#include <pthread.h>
 
 struct SensorData sd;
+
+void * httpThread(void *vargp){
+	
+	time_t timestamp;
+	
+	//Initialize HTTP library
+	HTTP_Init("sdp.ballistaline.com");
+	char request[1024];
+	char response[1024];
+	
+	timestamp = time(NULL);
+	
+	while(1){
+		if (difftime(time(NULL), timestamp) > 60) {
+			timestamp = time(NULL);
+			getGETstr(request, &sd);
+			HTTP_Get("dataReceiver.php", request, response, 1024);
+			printf("%s\n", response);
+		}
+	}
+}
 
 int main(int argc, char * argv[]) {
 
@@ -25,9 +47,11 @@ int main(int argc, char * argv[]) {
 	int return_val;
 	message_t msg;
 	time_t timestamp;
+	
+	pthread_t thread_id;
 
 	sensor_data_init(&sd);
-
+	
 
 	while (dev == NULL) {
 		dev = USBCom_Init();
@@ -37,27 +61,19 @@ int main(int argc, char * argv[]) {
 	}
 
 
-	//Initialize HTTP library
-	HTTP_Init("sdp.ballistaline.com");
-	char request[1024];
-	char response[1024];
-
-	//Start Time
-	timestamp = time(NULL);
-
-
 	// Set up data block 
 	strcpy(tx_data, "1:432.12\r\n");
 
+	pthread_create(&thread_id, NULL, httpThread, NULL);
 
 	while (1) {
 
 
-		return_val = USBCom_SendData(tx_data);
+		//return_val = USBCom_SendData(tx_data);
 
-		if (return_val != 0) {
-			printf("Did Not Recieve Data");
-		}
+		//if (return_val != 0) {
+		//	printf("Did Not Recieve Data");
+		//}
 
 		for (int i = 0; i < 64; i++)
 			rx_data[i] = 0;
@@ -65,15 +81,9 @@ int main(int argc, char * argv[]) {
 		if (USBCom_CheckReceivedData(rx_data)) {
 			msg = Protocol_DecodeInput(rx_data);
 			updateSensors(&msg, &sd);
-			//Protocol_PrintMessage(&msg);
+			Protocol_PrintMessage(&msg);
 			//printf("SD Flow Value: %f\n",sd.flow_measured);
 		}
 
-		if (difftime(time(NULL), timestamp) > 1) {
-			timestamp = time(NULL);
-			getGETstr(request, &sd);
-			HTTP_Get("dataReceiver.php", request, response, 1024);
-			printf("%s\n", response);
-		}
 	}
 }
